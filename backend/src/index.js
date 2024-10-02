2fc1c66929b01d98a70d20c98bb5f6302685caed
happy-7898 <singhharpreet5409@gmail.com>
2024-10-02 11:30:26 +0530

    added some utils files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
+import app from "./app.js";
 import dbConnect from "./db/index.js";
 import env from "dotenv";
 env.config();
 
 
 
-dbConnect();
+dbConnect()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server runnung at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection Failed");
+})
